a11538bb47de9bcb184ad6bf00d82988ae2e1498
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2022-11-21 17:04:04 -0500

    added code to change timebox color

--- script.js
+++ script.js
@@ -9,17 +9,40 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  
+  function saveTasks() {}
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  $(".container-fluid").children("div").each(timeColor);
+
+  function timeColor() {
+    let currentHour = dayjs().format("H");
+
+    for (i = 9; i <= 17; i++) {
+      if (i < currentHour) {
+        $("#hour-" + i).toggleClass("past");
+      }
+      if (i == currentHour) {
+        $("#hour-" + i).toggleClass("present");
+      }
+      if (i > currentHour) {
+        $("#hour-" + i).toggleClass("future");
+      }
+    }
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+  function printTasks() {}
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  let today = dayjs().format("ddd, MM/DD/YYYY");
+  document.getElementById("currentDay").innerHTML = today;
 });
